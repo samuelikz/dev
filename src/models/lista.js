b78e12bd8f22eb8ed68a572b1743752e8a39e61d
samuelikz <samuel.silva@a.ficr.edu.br>
2020-08-28 11:08:09 -0300

    Referencias de models

--- src/models/lista.js
+++ src/models/lista.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Lista.belongsTo(models.Usuario), {
+        targetKey: 'id',
+        as: 'usuarioId'
+      }
     }
   };
   Lista.init({
